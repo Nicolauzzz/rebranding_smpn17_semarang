2cd9f318525ea510a250ca1d57ec6ec5f218d7b9
Nicolauzzz <nicolausevanwidyatna@gmail.com>
2024-08-30 16:53:10 +0700

    2nd commit

---
+++ resources/views/perpustakaan.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PERPUSTAKAAN</title>
+</head>
+<body>
+<a href="/">BERANDA</a>
+<a href="/profil">PROFIL</a>
+<a href="/galeri">GALERI</a>
+<a href="/prestasi">PRESTASI</a>
+<a href="/perpustakaan">PERPUSTAKAAN</a>
+<a href="/lomba">LOMBA-LOMBA</a>
+<a href="/ppdb">PPDB 2024/2025</a>
+<a href="/kontak">KONTAK</a>
+<a href="/pengaduan">PENGADUAN</a>
+<a href="/berita">BERITA</a>
+
+<h1>PERPUSTAKAAN</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,42 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/welcome', function () {
-    return view('welcome');
+Route::get('/', function () {
+    return view('beranda');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/berita', function () {
+    return view('berita');
 });
 
-Route::get('/home', function () {
-    return view('home');
+Route::get('/galeri', function () {
+    return view('galeri');
+});
+
+Route::get('/kontak', function () {
+    return view('kontak');
+});
+
+Route::get('/lomba', function () {
+    return view('lomba');
+});
+
+Route::get('/pengaduan', function () {
+    return view('perpustakaan');
+});
+
+Route::get('/perpustakaan', function () {
+    return view('perpustakaan');
+});
+
+Route::get('/ppdb', function () {
+    return view('prestasi');
+});
+
+Route::get('/prestasi', function () {
+    return view('prestasi');
+});
+
+Route::get('/profil', function () {
+    return view('profil');
 });
